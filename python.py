3f5ecbd55a015cae93603b47bdc4280aa843f98f
Gustavo Costa de Oliveira <143671905+GustavoCdeOliveira@users.noreply.github.com>
2023-10-17 12:47:15 -0300

    python.py
    Utilizando Biblioteca pypdf2 para fazer junção de documentos em um único arquivo.
    
    Primeiro e necessário
    
    identificar a lista de arquivos que vai se unificar e depois uma função se para definir a extensão que vai se unificar no caso ".pdf" nesse exemplo
    
    feito isso e identificado os arquivos vai gerar um único no qual vai ficar com a nomenclatura PDF final.

---
+++ python.py
@@ -0,0 +1,15 @@
+import os
+
+import PyPDF2
+
+merger = PyPDF2.PdfMerger()
+
+lista_arquivos = os.listdir("arquivos")
+lista_arquivos.sort()
+print(lista_arquivos)
+
+for arquivo in lista_arquivos:
+    if ".pdf" in arquivo:
+        merger.append(f"arquivos/{arquivo}")
+
+merger.write("PDF Final.pdf")
